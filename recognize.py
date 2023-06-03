b2188d3acd4898b0256ff1787f581bd23cb0f5c7
Toan vip pro <ktoan515@gmail.com>
2023-06-03 13:08:28 +1100

    update the topic classification method in the video

--- recognize.py
+++ recognize.py
@@ -1,5 +1,6 @@
 # nthai: modify 02/05/2023
 # dktoan: modify 09/05/2023 update giam nhieu
+# dktoan: modify 15/05/2023 update phan loai chu de bang underthesea
 # original soucre: https://github.com/nestyme/Subtitles-generator
 import time
 import scipy.io.wavfile as wavfile
@@ -15,6 +16,13 @@ from datetime import datetime
 import noisereduce as nr
 import soundfile as sf
 import ffmpeg
+from underthesea import classify
+from langdetect import detect
+from googletrans import Translator
+# text-rank
+from summa import summarizer
+# tách câu
+from pyvi import ViTokenizer
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -26,6 +34,9 @@ def get_arguments():
                         help='step_time: default : 55')
     parser.add_argument('-noise','--algorithm_noise',
                         help="---> Chọn thuật toán giảm nhiễu",default="no")
+    parser.add_argument('-summary','--algorithm_summary',
+                        help="---> Chọn thuật toán dùng để tóm tắt văn bản",default="no")
+    # step_time = 50
     # step_time = 50
     arguments = parser.parse_args()
         
@@ -67,10 +78,47 @@ def recognize(wav_filename, args):
         os.remove(wav_filename)  
     with open( args.video +'_sub.txt', 'a', encoding='utf-8') as f:
         f.write(' {}'.format(result))
+    
+    # print(result)
+    translator = Translator()
+    # Kiểm tra ngôn ngữ của result
+    if detect(result) == 'vi':
+        result_trans = result
+    else:
+        # Dịch sang Tiếng Việt nếu đoạn văn bản không phải Tiếng Việt
+        result_trans = translator.translate(result, dest='vi').text
+        with open(args.video + '_sub_trans.txt', 'w', encoding='utf-8') as f:
+            f.write(result_trans)
+        if os.path.exists(args.video + '_sub_trans.txt'):
+            print("Lưu trữ kết quả thành công")
+        else:
+            print("Lưu trữ kết quả không thành công")  
+
+    summary = args.algorithm_summary
+    if summary == 'lexrank':
+        print("Sử dụng thuật toán lexrank")
+    elif summary == 'textrank':
+        result_summary_text = summarizer.summarize(result)
+        if result_summary_text == '':
+            print('Tóm tắt thất bại !!! văn bản trong video quá ngắn')
+        else:
+            print("đang tóm tắt văn bản trong video")
+            with open(args.video + '_sub_summarization.txt', 'w', encoding='utf-8') as file:
+                file.write(result_summary_text)
+            print('Text-summarization: '+ result_summary_text)
+            print('Đã lưu file thành công tại đường dẫn:', args.video + '_sub_summarization.txt')
+    else:
+        print("Không sử dụng thuật toán tóm tắt nội dung video")
+        pass
+    topic = '_'.join(classify(result))
+    print("Chu de cua video la:", topic)
+    
+    end = time.time()
     print("path wav:" + wav_filename)
     print("path txt:" +args.video +'_sub.txt')
-   #  return result
-    
+    # print("path text summary:" +args.video +'_sub.txt')
+    print('elapsed time: {}'.format(end - start))
+    exit()
 def get_audio(video):
     os.system('ffmpeg -y  -threads 4 -i {} -f wav -ab 192000 -vn {}'.format(video, 'tmp/current.wav'))
     
@@ -135,6 +183,11 @@ def videoOutput(file_in,file_srt,file_out):
         os.system('ffmpeg -y -i {} -filter_complex "subtitles={}" {}'.format(file_in,file_srt,file_out))
 
 
+def split_sentences(text):
+    sentences = ViTokenizer.tokenize(text).split('.')
+    sentences = [s.strip() + '.' for s in sentences if s.strip()]
+    return sentences
+
 if __name__ == '__main__':
     from time import gmtime, strftime
     time_text = str(strftime("%Y%m%d_%H%M%S", gmtime())) 
@@ -168,26 +221,27 @@ if __name__ == '__main__':
         noises = args.algorithm_noise
         if noises:
             # Giảm nhiễu dùng thuật toán deepfilter
-            if noises == 'deep':
+            if noises == 'noise':
+                print("Sử dụng thuật toán NoiseReduce để giảm nhiễu")
+                newfile = folder+'/noise.wav'
+                noise_reduce(file,newfile)
+                # wav_to_flac(path+newname+'.wav',path+newname+'.flac')
+            elif noises == 'deep':
                 print("Sử dụng thuật toán DeepFilterNet để giảm nhiễu")
                 noise_deepfilternet(file,folder)
                 newfile = folder+'/deep.wav'
                 rename(folder+'/0_DeepFilterNet2.wav',newfile)
                 # wav_to_flac(path+newname+'.wav',path+newname+'.flac')
-                pass
             # Giải thuật giảm nhiễu Noisereduce (không cố định)
-            elif noises == 'noise':
-                print("Sử dụng thuật toán NoiseReduce để giảm nhiễu")
-                newfile = folder+'/noise.wav'
-                noise_reduce(file,newfile)
-                # wav_to_flac(path+newname+'.wav',path+newname+'.flac')
             else:
             # Không chọn giải thuật
                 # rename(path+name+'.wav',path+newname+'.wav')
                 print("Không sử dụng thuật toán giảm nhiễu")
                 pass
         # source = path+newname+'.wav'
+        
+
+
         recognize(newfile,args)
-    end = time.time()
-    print('elapsed time: {}'.format(end - start))
+    
     # os.system('rm tmp/*')
